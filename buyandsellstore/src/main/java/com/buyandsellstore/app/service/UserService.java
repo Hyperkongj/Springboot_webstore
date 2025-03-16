09f93c29cd4f166daf7411ac2d91aeb8a639cf41
svb46 <svb46@txstate.edu>
2025-03-16 12:26:34 -0500

    Signup related validation changes in frontend and backend

--- buyandsellstore/src/main/java/com/buyandsellstore/app/service/UserService.java
+++ buyandsellstore/src/main/java/com/buyandsellstore/app/service/UserService.java
@@ -1,5 +1,6 @@
 package com.buyandsellstore.app.service;
 
+import com.buyandsellstore.app.dto.AuthResponse;
 import com.buyandsellstore.app.model.User;
 import com.buyandsellstore.app.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,12 +17,18 @@ public class UserService {
         return user != null && user.getPassword().equals(password) ? user : null; // Ideally hash the password
     }
 
-    public User createUser(User user) {
+    public AuthResponse createUser(User user) {
 //        System.out.println(user.toString());
 //        System.out.println(user.getUsername());
-//        if (userRepository.existsByUsername(user.getUsername())) {
-//            return null;
-//        }
-        return userRepository.save(user);
+        if (userRepository.existsByUsername(user.getUsername())) {
+            return new AuthResponse(false, "Username already exists!", null);
+        }
+        if (userRepository.existsByEmail(user.getEmail())) {
+            return new AuthResponse(false, "Email already exists!", null);
+        }
+        if (userRepository.existsByPhone(user.getPhone())) {
+            return new AuthResponse(false, "Phone number already exists!", null);
+        }
+        return new AuthResponse(true, "User successfully added to DB!", userRepository.save(user));
     }
 }
